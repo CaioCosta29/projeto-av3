2456e9805ec6b1fcd1b892cffa5cc81e65011e33
CaioCosta29 <caiocostacb@gmail.com>
2025-04-09 14:51:17 -0300

    Projeto

---
+++ src/main/java/com/projeto_av3/projeto_av3/ProjetoAv3Application.java
@@ -0,0 +1,13 @@
+package com.projeto_av3.projeto_av3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAv3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAv3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto_av3/projeto_av3/ProjetoAv3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto_av3.projeto_av3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAv3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
